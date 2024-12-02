fb98c4ed0afb51f70c68ff9116719b7b21353657
MikhilExplorer <ailani.mikhil@gmail.com>
2024-12-02 16:15:34 +0530

    third

--- app.js
+++ app.js
@@ -36,3 +36,16 @@ gsap.to(".main",{
         scrub:2
     }
 })
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+  });
